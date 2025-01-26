1521fe2a5e47cb7519fd8e2bb727e628728a56d4
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2025-01-26 12:17:16 -0300

    Merge web3, types, tcp-types into qubic-rs

---
+++ qubic-rs/src/lib.rs
@@ -0,0 +1,13 @@
+#![allow(clippy::let_underscore_future)]
+#![allow(clippy::needless_range_loop)]
+#![allow(async_fn_in_trait)]
+
+
+pub mod transport;
+pub mod client;
+
+pub extern crate qubic_tcp_types;
+pub extern crate qubic_types;
+
+#[cfg(test)]
+mod tests;
